75bc897ac8cdda2307069e8746c1cb942404cfa9
karnerb <blaz.karner@outlook.com>
2022-09-19 19:49:59 +0200

    templated logger

--- basic_example/logger.h
+++ basic_example/logger.h
@@ -1,11 +1,42 @@
 #pragma once
 #include <fstream>
-#include "llmodel.h"
-class Logger{
+/* template <typename T> class Logger{
     public:
     std::ofstream file;
-    LL_model& model;
-    Logger(std::string output_file, LL_model& model);
+    T& model;
+    Logger(std::string output_file, T& model);
     ~Logger();
     void log_params();
+}; */
+
+template <typename T> class Logger{
+    public:
+    T& model;
+    std::ofstream file;
+
+    
+    Logger(std::string filename, T& model) : model(model){
+
+    file.open(filename);
+    file << "n: " << model.n << "\n";
+    file << "beta" << " " << "cycle" << " " << "acceptance_rate" << " " << "Energy" << " "
+        << "P2_00" << " " << "P2_01" << " " << "P2_02" << " "
+        << "P2_10" << " " << "P2_11" << " " << "P2_12" << " "
+        << "P2_20" << " " << "P2_21" << " " << "P2_22"
+        << "\n"; 
+
+    }
+
+
+    ~Logger(){
+        file.close();
+    }
+
+    void log_params(){
+        file << model.beta << " " << model.cycle << " " << model.acceptance_rate << " " << model.E << " "
+        << model.P2[0][0] << " " << model.P2[0][1] << " " << model.P2[0][2] << " "
+        << model.P2[1][0] << " " << model.P2[1][1] << " " << model.P2[1][2] << " "
+        << model.P2[2][0] << " " << model.P2[2][1] << " " << model.P2[2][2]
+        << "\n"; 
+    }
 };
