cdd31fcf93e518f9557e9b01d3e372b2bb5f2f3b
karnerb <blaz.karner@outlook.com>
2022-11-03 13:40:37 +0100

    moved rotation angle

--- code/llmodelBM/llmodelBM_lattice_initialization.cpp
+++ code/llmodelBM/llmodelBM_lattice_initialization.cpp
@@ -65,7 +65,7 @@ void LL_model_BM::break_molecule(int I){
 
     
     //rotate the spins by +-alpha/2 about (v0, v1, v2)
-    double alpha = pi / 3;
+    
     n0 = v[0];
     n1 = v[1];
     n2 = v[2];        
