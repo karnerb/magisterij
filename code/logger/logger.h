7fb63e0ce200fff1c277490da600d875f27b8109
karnerb <blaz.karner@outlook.com>
2022-11-08 09:23:11 +0100

    updated logger to output lattice config in single file

--- code/logger/logger.h
+++ code/logger/logger.h
@@ -31,21 +31,19 @@ template <typename T> class Logger{
     }
 
     void output_lattice_configuration(){
-        std::ofstream lattice_broken_sites;
         std::ofstream lattice_configuration;
         std::string name = filename;
-        lattice_broken_sites.open("output/lattice_config/" + name.erase(name.length()-4) + "_broken_sites_" + std::to_string(i_lattice) + ".txt");
-        if (!lattice_broken_sites.is_open()) std::cout << "Error opening output file!\n";
-        lattice_configuration.open("output/lattice_config/" + name + "_lattice_config_" + std::to_string(i_lattice) + ".txt");
+        lattice_configuration.open("output/lattice_config/" + name.erase(name.length()-4) + "_lattice_config_" + std::to_string(i_lattice) + ".txt");
         if (!lattice_configuration.is_open()) std::cout << "Error opening output file!\n";
-        lattice_broken_sites << 1.0/model.beta << " " << model.n << " " 
+        lattice_configuration << 1.0/model.beta << " " << model.n << " " 
                      << model.count_broken_molecules() << " " 
                      << model.broken_neighbors << " " 
                      << model.broken_cluster_count <<
                       "\n";
+        lattice_configuration << "broken v1x v1y v1z v2x v2y v2z \n";
         for (int i=0; i<model.n*model.n*model.n; i++){
-            lattice_broken_sites << model.broken[i] << "\n";
-            lattice_configuration << model.spins[i][0][0] << " " 
+            lattice_configuration << model.broken[i] << " "
+                                  << model.spins[i][0][0] << " " 
                                   << model.spins[i][0][1] << " "
                                   << model.spins[i][0][2] << " " 
                                   << model.spins[i][1][0] << " " 
@@ -53,7 +51,7 @@ template <typename T> class Logger{
                                   << model.spins[i][1][2] << " " 
                                   << "\n";
         }
-        lattice_broken_sites.close();
+
         lattice_configuration.close();
         i_lattice++;
     }
