b6e66df07aab9accc46c3cbb51ee16ae581d76c1
karnerb <blaz.karner@outlook.com>
2022-11-15 10:59:54 +0100

    updated elastomer output

--- code/elastomer/LC_Elastomer_output.cpp
+++ code/elastomer/LC_Elastomer_output.cpp
@@ -9,12 +9,17 @@ std::string LC_Elastomer::metadata(){
 
 std::string LC_Elastomer::names(){
         std::string space = " ";
-        std::string names = "beta" + space + "cycle" + space + "acceptance_rate" + space + "Energy" + space
+        std::string names = 
+        "beta" + space 
+        + "cycle" + space 
+        + "acceptance_rate" + space 
+        + "EnergyLL" + space
+        + "EnergyElastic" + space
+        + "EnergyCoupling" + space
         + "P2_00" + space + "P2_01" + space + "P2_02" + space
         + "P2_10" + space + "P2_11" + space + "P2_12" + space
         + "P2_20" + space + "P2_21" + space + "P2_22" + space
-        + "polar_order" + space + "same_neighbors" + space
-        + "swap_acceptance_rate" + space
+        + "same_neighbors" + space
         + "broken_cluster_count" + space
         + "lambda" + space
         + "sigma" 
@@ -23,12 +28,16 @@ std::string LC_Elastomer::names(){
 }
 
 std::ostream& operator <<(std::ostream& os, const LC_Elastomer& model){
-    os  << model.beta << " " << model.cycle << " " << model.acceptance_rate << " " << model.Energy_LL_interaction << " "
+    os  << model.beta << " " 
+        << model.cycle << " " 
+        << model.acceptance_rate << " "
+        << model.Energy_LL_interaction << " "
+        << model.Energy_elastic << " "
+        << model.Energy_coupling << " "
         << model.P2[0][0] << " " << model.P2[0][1] << " " << model.P2[0][2] << " "
         << model.P2[1][0] << " " << model.P2[1][1] << " " << model.P2[1][2] << " "
         << model.P2[2][0] << " " << model.P2[2][1] << " " << model.P2[2][2] << " "
-        << model.polar_order << " " << model.broken_neighbors << " "
-        << model.swap_acceptance_rate << " " 
+        << model.broken_neighbors << " "
         << model.broken_cluster_count << " "
         << model.lambda << " " 
         << model.sigma  
